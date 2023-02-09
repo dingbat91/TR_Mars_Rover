06cb67aab45eca13eab00260cb60176d9c06c3e0
Matthew Hanson <dingbat91@gmail.com>
2023-02-09 10:13:58 +0000

    Initial Menu setup

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import inquirer from "inquirer";
+import inquirer, { Answers, ListQuestion, Question } from "inquirer";
+import Choice from "inquirer/lib/objects/choice";
 
 /**
  * Main menu
@@ -8,6 +9,42 @@ import inquirer from "inquirer";
  * for menu customisation please refer to instructions located here: https://www.npmjs.com/package/inquirer
  */
 
-console.log("Welcome to the Mars Rover Program");
-console.log("================================");
-console.log("Made by Matthew Hanson");
+async function menu() {
+	//initial root menu choices
+	const mainMenuChoices: Choice[] = [
+		{
+			name: "new game",
+			value: "new",
+			short: "new",
+			disabled: false,
+		},
+		{
+			name: "quit",
+			value: "quit",
+			short: "quit",
+			disabled: false,
+		},
+	];
+
+	//Menu options object, add new options here for the menu layout
+	const menuOptions: ListQuestion[] = [
+		{
+			type: "list",
+			name: "mainMenuChoice",
+			message: "Main menu",
+			choices: mainMenuChoices,
+			prefix: "",
+		},
+	];
+
+	console.log("Welcome to the Mars Rover Program");
+	console.log("================================");
+	console.log("Made by Matthew Hanson");
+	const RESULT = await inquirer.prompt(menuOptions).catch((error) => {
+		console.log(error);
+		return -1;
+	});
+	console.log(RESULT);
+}
+
+menu();
