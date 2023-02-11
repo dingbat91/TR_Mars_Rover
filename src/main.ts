5d643e2f531f7fe731575a2833fb420588155135
Matthew Hanson <dingbat91@gmail.com>
2023-02-11 09:30:46 +0000

    Addition of Audio and Mars image to opening screen.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,46 @@
+import Audic from "audic";
+import terminalkit from "terminal-kit";
 import { GameMenu } from "./menus/GameMenu";
 import { MainMenu } from "./menus/MainMenu";
 import { GameGrid } from "./types/grid/grid";
 import { Rover } from "./types/vehicle/vehicle";
 
 async function main() {
+	//Opening Screen Loading
+	const terminal = terminalkit.terminal;
+	await terminal.drawImage("src\\media\\mars.png", {
+		shrink: { width: 40, height: 40 },
+	});
+	//------------------
+	//Audio Code--------
+	const audic = new Audic("\\src\\media\\audio\\StarTrek.mp3");
+	audic.volume = 0.3;
+	audic.currentTime = 5000;
+	await audic.play().catch((err) => {
+		console.log("There was an error! No cool music for you :(");
+	});
+	//------------------
+	//menu code---------
 	let menuResult: string = await MainMenu();
-
 	switch (menuResult) {
 		case "new": {
-			gameloop(new GameGrid());
+			audic.pause();
+			audic.destroy();
+			GameLoop(new GameGrid());
 			break;
 		}
 		case "exit": {
 			console.log("Goodbye!");
-			return 0;
-			break;
+			audic.pause();
+			audic.destroy();
+			return;
 		}
 	}
+	//-----------------
 }
 
 //UI functionality - not unit tested
-async function gameloop(grid: GameGrid) {
+async function GameLoop(grid: GameGrid) {
 	let activeRover = new Rover(grid);
 	activeRover.initVic();
 	let running = true;
